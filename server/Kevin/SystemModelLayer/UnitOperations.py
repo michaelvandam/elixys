70bc4f03ff5dab637b810fd43226c8b58544c05b
Kevin Quinn <kquinn@mednet.ucla.edu>
2011-06-13 11:54:37 -0700

    Updated UnitOperations with 'waitForCondition'.
    Added many new actions to the operations code to account for multiple steps.
    Need to add a timeout in the waitForCondition function to avoid infinite loops/system lockup.

--- server/Kevin/SystemModelLayer/UnitOperations.py
+++ server/Kevin/SystemModelLayer/UnitOperations.py
@@ -3,8 +3,23 @@
 Elixys Unit Operations
 """
 import Thread from threading
+
+ZUP = "ZUP"
+ZDOWN = "ZDOWN"
+XEVAPORATE = "XEVAPORATE"
+XREACTA = "XREACTA"
+XREACTB = "XREACTB"
+XADDREAGENT = "XADDREAGENT"
+XTRANSFER = "XTRANSFER"
+XVIALREMOVE = "XVIALREMOVE"
+XRADIATION = "XRADIATION"
+EQUAL = "="
+GREATER = ">"
+LESS = "<"
+
 class UnitOperation(Thread):
   def __init__(self,systemModel):
+    Thread.__init__(self)
     self.params = {"param":"value"}
     self.systemModel = systemModel
     self.steps = ("step1","step2")
@@ -32,15 +47,30 @@ class UnitOperation(Thread):
       print "unit operation complete"
     else:
       print("Self.steps does not exist")# This will be an error log
+  def waitForCondition(function,condition,comparator):
+    if comparator == EQUAL:
+      while not(function() == condition):
+        pass
+    elif comparator == GREATER:
+      while not(function() >= condition):
+        pass
+    elif comparator == LESS:
+        while not(function() <= condition):
+        pass
+    
+  def timeDelay(self):
+    time.sleep(self.Timer)
+    
   def resume(self):
-    pass
+    self.pause = False
   def pause(self):
     self.pause = True
   def abort():
-    pass
+    #Safely abort -> Do not move, turn off heaters, turn set points to zero.
+    self.systemModel[self.ReactorID]['temperature_controller'].setTemperature(0)
+    
   def getTotalSteps(self):
     return self.steps #Integer
-
   def isRunning(self):
     return self.isRunning
     
@@ -65,9 +95,9 @@ class React(UnitOperation):
     self.params = params #Should have parameters listed below
     #self.ReactorID
     #self.Temp
-    #self.Time
+    #self.Timer
     #self.coolTemp
-    #self.positionName
+    #self.reactPosition
   def run(self):
     self.steps=["Starting react operation","Moving to reaction position","Heating reactor","Waiting for reaction","Cooling reactor"]
     self.doNext()
@@ -75,33 +105,35 @@ class React(UnitOperation):
     self.doNext()
     self.setTemp()
     self.doNext()
+    self.startHeating()
     self.timeDelay()
     self.doNext()
-    self.setCool(ON)  #In Progress
-    self.setCool(OFF) #In Progress
+    self.setCool()
     self.doNext()
     
   def setReactorPosition(self):
-    self.systemModel[self.ReactorID]['motion'].setPosition(self.positionName)
-    self.waitForCondition(self.systemModel[self.ReactorID]['motion'].getPosition(),self.positionName)
+    self.systemModel[self.ReactorID]['motion'].setPosition(ZDOWN)
+    self.waitForCondition(self.systemModel[self.ReactorID]['motion'].getCurrentPosition,ZDOWN,EQUAL)
+    self.systemModel[self.ReactorID]['motion'].setPosition(self.reactPosition)
+    self.waitForCondition(self.systemModel[self.ReactorID]['motion'].getCurrentPosition,self.reactPosition,EQUAL)
+    self.systemModel[self.ReactorID]['motion'].setPosition(ZUP)
+    self.waitForCondition(self.systemModel[self.ReactorID]['motion'].getPosition,ZUP,EQUAL)
 
-  def getTemperatureController(): #Not sure why we need this?
+  def getTemperatureController(): #Do we need this if we have the entire system model at our disposal?
     return self.systemModel[self.ReactorID]['temperature_controller']
-    
+  
   def setTemp(self):
     self.systemModel[self.ReactorID]['temperature_controller'].setTemperature(self.Temp)
-    self.waitForCondition(self.systemModel[self.ReactorID]['temperature_controller'].getTemperature(),self.Temp)
-    
-  def getTemp(self):
-    self.systemModel[self.ReactorID]['temperature_controller'].getCurrentTemperature()
-    
-  def timeDelay(self):
-    self.timeDelay(self.Time)
-  
-  def setCool(self,pumpState):#In Progress
-    self.systemModel[self.ReactorID]['temperature_controller'].setTemperature(self.Temp)
-    self.systemModel[self.ReactorID]['cooling_system'].setCooling(pumpState)#In Progress
-    self.waitForCondition(self.systemModel[self.ReactorID]['temperature_controller'].getTemperature(),self.coolTemp)
+    self.waitForCondition(self.systemModel[self.ReactorID]['temperature_controller'].getCurrentTemperature,self.Temp,GREATER)
+   
+  def setCool(self):
+    self.systemModel[self.ReactorID]['temperature_controller'].setHeaterState(OFF)
+    self.waitForCondition(self.systemModel[self.ReactorID]['temperature_controller'].getHeaterState,OFF,EQUAL)
+    self.systemModel[self.ReactorID]['cooling_system'].setCooling(ON)
+    self.waitForCondition(self.systemModel[self.ReactorID]['cooling_system'].getCooling,ON,EQUAL)
+    self.waitForCondition(self.systemModel[self.ReactorID]['temperature_controller'].getTemperature(),self.coolTemp) 
+    self.systemModel[self.ReactorID]['cooling_system'].setCooling(OFF)
+    self.waitForCondition(self.systemModel[self.ReactorID]['cooling_system'].getCooling,OFF,EQUAL)
 
 class AddReagent(UnitOperation):
   def __init__(self,systemModel,params):
@@ -109,7 +141,7 @@ class AddReagent(UnitOperation):
     self.params = params #Should have parameters listed below
     #self.ReactorID
     #self.ReagentID
-    #self.loadPosition
+    #self.reactPosition
 
   def run(self):
     self.steps=["Starting add reagent operation","Moving to addition position","Adding reagent","Removing empty vial"]
@@ -123,16 +155,43 @@ class AddReagent(UnitOperation):
     self.doNext()
   
   def setReactorPosition(self):
-    self.systemModel[self.ReactorID]['motion'].setPosition(REAGENT)
-    self.waitForCondition(self.systemModel[self.ReactorID]['motion'].getPosition(),REAGENT)
+    self.systemModel[self.ReactorID]['motion'].setPosition(ZDOWN)
+    self.waitForCondition(self.systemModel[self.ReactorID]['motion'].getCurrentPosition,ZDOWN,EQUAL)
+    self.systemModel[self.ReactorID]['motion'].setPosition(self.reagentPosition)
+    self.waitForCondition(self.systemModel[self.ReactorID]['motion'].getCurrentPosition,self.reagentPosition,EQUAL)
+    self.systemModel[self.ReactorID]['motion'].setPosition(ZUP)
+    self.waitForCondition(self.systemModel[self.ReactorID]['motion'].getPosition,ZUP,EQUAL)
 
   def setGripperPlace():
-    self.systemModel[self.Gripper].setPosition(self.ReagentID,self.loadPosition)
-    self.waitForCondition( self.systemModel[self.Gripper].getPosition(),self.ReagentID)
+    self.systemModel[self.Gripper].setPosition(ZUP)
+    self.waitForCondition(self.systemModel[self.Gripper].getPosition,ZUP,EQUAL)
+    self.systemModel[self.Gripper].setCoordinate(self.ReagentID)
+    self.waitForCondition(self.systemModel[self.Gripper].getCoordinate,self.ReagentID,EQUAL)
+    self.systemModel[self.Gripper].setPosition(ZDOWN)
+    self.waitForCondition(self.systemModel[self.Gripper].getPosition,ZDOWN,EQUAL)
+    self.systemModel[self.Gripper].setGripperState(GRIP)
+    self.waitForCondition(self.systemModel[self.Gripper].getGripperState,GRIP,EQUAL)
+    self.systemModel[self.Gripper].setPosition(ZUP)
+    self.waitForCondition(self.systemModel[self.Gripper].getPosition,ZUP,EQUAL)
+    self.systemModel[self.Gripper].setCoordinate(self.reactPosition)
+    self.waitForCondition(self.systemModel[self.Gripper].getCoordinate,self.reactPosition,EQUAL)
+    self.systemModel[self.Gripper].setPosition(ZDOWN)
+    self.waitForCondition(self.systemModel[self.Gripper].getPosition,ZDOWN,EQUAL)
     
-  def setGripperPlace():
-    self.systemModel[self.Gripper].setRemovePosition(self.ReagentID)
-    self.waitForCondition( self.systemModel[self.Gripper].getRemovePosition(),self.ReagentID)
+  def setGripperRemove():
+    self.waitForCondition(self.systemModel[self.Gripper].getCoordinate,self.reactPosition,EQUAL)#Make sure we are in the right location.
+    self.waitForCondition(self.systemModel[self.Gripper].getPosition,ZDOWN,EQUAL)#Make sure we are in the down position.
+    self.systemModel[self.Gripper].setGripperState(GRIP)
+    self.waitForCondition(self.systemModel[self.Gripper].getGripperState,GRIP,EQUAL)
+    self.systemModel[self.Gripper].setPosition(ZUP)
+    self.waitForCondition(self.systemModel[self.Gripper].getPosition,ZUP,EQUAL)
+    self.systemModel[self.Gripper].setCoordinate(self.ReagentID)
+    self.waitForCondition(self.systemModel[self.Gripper].getCoordinate,self.ReagentID,EQUAL)
+    self.systemModel[self.Gripper].setPosition(ZDOWN)
+    self.waitForCondition(self.systemModel[self.Gripper].getPosition,ZDOWN,EQUAL)
+    self.systemModel[self.Gripper].setGripperState(UNGRIP)
+    self.waitForCondition(self.systemModel[self.Gripper].getGripperState,UNGRIP,EQUAL)
+    
 
 class Evaporate(UnitOperation):
   def __init__(self,systemModel,params):
@@ -140,40 +199,55 @@ class Evaporate(UnitOperation):
     self.params = params #Should have parameters listed below
     #self.ReactorID
     #self.Temp
-    #self.Time
+    #self.Timer
     #self.coolTemp
   def run(self):
-    self.steps=["Starting evaporate operation","Moving to evaporation position","Heating reactor","Waiting for evaporation","Cooling reactor"]
+    self.steps=["Starting evaporate operation","Moving to evaporation position","Setting heater set point","Heating reactor","Waiting for evaporation","Cooling reactor"]
     self.doNext()
     self.setReactorPosition()
     self.doNext()
     self.setTemp()
     self.doNext()
+    self.startHeating()
     self.timeDelay()
     self.doNext()
     self.setCool()
     self.doNext()
   
   def setReactorPosition(self):
-    self.systemModel[self.ReactorID]['motion'].setPosition(EVAPORATE)
-    self.waitForCondition(self.systemModel[self.ReactorID]['motion'].getPosition(),EVAPORATE)
+    self.systemModel[self.ReactorID]['motion'].setPosition(ZDOWN)
+    self.waitForCondition(self.systemModel[self.ReactorID]['motion'].getCurrentPosition,ZDOWN,EQUAL)
+    self.systemModel[self.ReactorID]['motion'].setPosition(XEVAPORATE)
+    self.waitForCondition(self.systemModel[self.ReactorID]['motion'].getCurrentPosition,XEVAPORATE,EQUAL)
+    self.systemModel[self.ReactorID]['motion'].setPosition(ZUP)
+    self.waitForCondition(self.systemModel[self.ReactorID]['motion'].getPosition,ZUP,EQUAL)
 
   def getTemperatureController(): #Not sure why we need this?
     return self.systemModel[self.ReactorID]['temperature_controller']
     
   def setTemp(self):
-    self.systemModel[self.ReactorID]['temperature_controller'].setTemperature(self.Temp)
-    self.waitForCondition(self.systemModel[self.ReactorID]['temperature_controller'].getTemperature(),self.Temp)
+    self.systemModel[self.ReactorID]['temperature_controller'].setSetPoint(self.Temp)
+    self.waitForCondition(self.systemModel[self.ReactorID]['temperature_controller'].getSetPoint,self.Temp,GREATER)
+    
+  def startHeating(self):
+    self.systemModel[self.ReactorID]['temperature_controller'].setHeaterState(ON)
+    self.waitForCondition(self.systemModel[self.ReactorID]['temperature_controller'].getHeaterState,ON,EQUAL)
+    self.waitForCondition(self.systemModel[self.ReactorID]['temperature_controller'].getCurrentTemperature,self.Temp,GREATER)
+  
+  def setCool(self):
+    self.systemModel[self.ReactorID]['temperature_controller'].setHeaterState(OFF)
+    self.waitForCondition(self.systemModel[self.ReactorID]['temperature_controller'].getHeaterState,OFF,EQUAL)
+    self.systemModel[self.ReactorID]['cooling_system'].setCooling(ON)
+    self.waitForCondition(self.systemModel[self.ReactorID]['cooling_system'].getCooling,ON,EQUAL)
+    self.waitForCondition(self.systemModel[self.ReactorID]['temperature_controller'].getTemperature(),self.coolTemp) 
+    self.systemModel[self.ReactorID]['cooling_system'].setCooling(OFF)
+    self.waitForCondition(self.systemModel[self.ReactorID]['cooling_system'].getCooling,OFF,EQUAL)
     
   def getTemp(self):
     self.systemModel[self.ReactorID]['temperature_controller'].getCurrentTemperature()
     
   def timeDelay(self):
     self.timeDelay(self.Time)
-  
-  def setCool(self):
-    self.systemModel[self.ReactorID]['temperature_controller'].setCool(self.coolTemp)       
-    self.waitForCondition(self.systemModel[self.ReactorID]['temperature_controller'].getTemperature(),self.coolTemp) 
     
 class InstallVial(UnitOperation):
   def __init__(self,systemModel,params):
@@ -187,8 +261,12 @@ class InstallVial(UnitOperation):
     self.doNext()
   
   def setReactorPosition(self):
-    self.systemModel[self.ReactorID]['motion'].setPosition(INSTALL)
-    self.waitForCondition(self.systemModel[self.ReactorID]['motion'].getPosition(),INSTALL)
+    self.systemModel[self.ReactorID]['motion'].setPosition(ZDOWN)
+    self.waitForCondition(self.systemModel[self.ReactorID]['motion'].getCurrentPosition,ZDOWN,EQUAL)
+    self.systemModel[self.ReactorID]['motion'].setPosition(XINSTALL)
+    self.waitForCondition(self.systemModel[self.ReactorID]['motion'].getCurrentPosition,XINSTALL,EQUAL)
+    self.systemModel[self.ReactorID]['motion'].setPosition(ZUP)
+    self.waitForCondition(self.systemModel[self.ReactorID]['motion'].getPosition,ZUP,EQUAL)
     
 class TransferToHPLC(UnitOperation):
   def __init__(self,systemModel,params):
@@ -207,8 +285,13 @@ class TransferToHPLC(UnitOperation):
     self.doNext()
   
   def setReactorPosition(self):
-    self.systemModel[self.ReactorID]['motion'].setPosition(TRANSFER)
-    self.waitForCondition(self.systemModel[self.ReactorID]['motion'].getPosition(),TRANSFER)
+    self.systemModel[self.ReactorID]['motion'].setPosition(ZDOWN)
+    self.waitForCondition(self.systemModel[self.ReactorID]['motion'].getCurrentPosition,ZDOWN,EQUAL)
+    self.systemModel[self.ReactorID]['motion'].setPosition(XTRANSFER)
+    self.waitForCondition(self.systemModel[self.ReactorID]['motion'].getCurrentPosition,XTRANSFER,EQUAL)
+    self.systemModel[self.ReactorID]['motion'].setPosition(ZUP)
+    self.waitForCondition(self.systemModel[self.ReactorID]['motion'].getPosition,ZUP,EQUAL)
+    
 
   def setTransfer(self):
     self.systemModel[self.ReactorID]['transfer'].setTransfer(HPLC)
@@ -236,8 +319,12 @@ class TransferElute(UnitOperation):
     self.doNext()
   
   def setReactorPosition(self):
-    self.systemModel[self.ReactorID]['motion'].setPosition(TRANSFER)
-    self.waitForCondition(self.systemModel[self.ReactorID]['motion'].getPosition(),TRANSFER)
+    self.systemModel[self.ReactorID]['motion'].setPosition(ZDOWN)
+    self.waitForCondition(self.systemModel[self.ReactorID]['motion'].getCurrentPosition,ZDOWN,EQUAL)
+    self.systemModel[self.ReactorID]['motion'].setPosition(XTRANSFER)
+    self.waitForCondition(self.systemModel[self.ReactorID]['motion'].getCurrentPosition,XTRANSFER,EQUAL)
+    self.systemModel[self.ReactorID]['motion'].setPosition(ZUP)
+    self.waitForCondition(self.systemModel[self.ReactorID]['motion'].getPosition,ZUP,EQUAL)
   
   def setStopcock(self):
     self.systemModel[self.ReactorID]['stopcock'].setStopcock(self.stopcockPosition)       
@@ -252,23 +339,37 @@ class Transfer(UnitOperation):
     UnitOperation.__init__(self,systemModel)
     self.params = params #Should have parameters listed below
     #self.ReactorID
-    #self.transferPosition
+    #self.transferReactorID
     
   def run(self):
     self.steps=["Starting Transfer operation","Moving to transfer position","Transferring product"]
     self.doNext()
     self.setReactorPosition()
     self.doNext()
-    self.setTransfer()
+    self.setTransferReactorPosition()
+    self.startTransfer()
     self.doNext()
   
   def setReactorPosition(self):
-    self.systemModel[self.ReactorID]['motion'].setPosition(TRANSFER)
-    self.waitForCondition(self.systemModel[self.ReactorID]['motion'].getPosition(),TRANSFER)
+    self.systemModel[self.ReactorID]['motion'].setPosition(ZDOWN)
+    self.waitForCondition(self.systemModel[self.ReactorID]['motion'].getCurrentPosition,ZDOWN,EQUAL)
+    self.systemModel[self.ReactorID]['motion'].setPosition(XTRANSFER)
+    self.waitForCondition(self.systemModel[self.ReactorID]['motion'].getCurrentPosition,XTRANSFER,EQUAL)
+    self.systemModel[self.ReactorID]['motion'].setPosition(ZUP)
+    self.waitForCondition(self.systemModel[self.ReactorID]['motion'].getPosition,ZUP,EQUAL)
+    
+  def setTransferReactorPosition(self):
+    self.systemModel[self.transferReactorID]['motion'].setPosition(ZDOWN)
+    self.waitForCondition(self.systemModel[self.transferReactorID]['motion'].getCurrentPosition,ZDOWN,EQUAL)
+    self.systemModel[self.transferReactorID]['motion'].setPosition(XADDREAGENT)
+    self.waitForCondition(self.systemModel[self.transferReactorID]['motion'].getCurrentPosition,XADDREAGENT,EQUAL)
+    self.systemModel[self.transferReactorID]['motion'].setPosition(ZUP)
+    self.waitForCondition(self.systemModel[self.transferReactorID]['motion'].getPosition,ZUP,EQUAL)
 
-  def setTransfer(self):
+  def startTransfer(self):
     self.systemModel[self.ReactorID]['transfer'].setTransfer(self.transferPosition)
-    self.waitForCondition(self.systemModel[self.ReactorID]['transfer'].getTransfer(),self.transferPosition)
+    
+    self.waitForCondition(self.systemModel[self.ReactorID]['transfer'].getTransfer,self.transferPosition,,EQAL)
  
 class UserInput(UnitOperation):
   def __init__(self,systemModel,params):
@@ -278,20 +379,17 @@ class UserInput(UnitOperation):
     #self.isCheckbox
     
   def run(self):
-    self.steps=["Starting Transfer operation","Moving to transfer position","Transferring product"]
+    self.steps=["Starting User Input operation","Waiting for OK"]
     self.doNext()
-    self.setReactorPosition()
+    self.setMessageBox()
     self.doNext()
-    self.setTransfer()
-    self.doNext()
-  
-  def setReactorPosition(self):
-    self.systemModel[self.ReactorID]['motion'].setPosition(self.positionName)
-    self.waitForCondition(self.systemModel[self.ReactorID]['motion'].getPosition(),self.positionName)
-
-  def setTransfer(self):
-    self.systemModel[self.ReactorID]['transfer'].setTransfer(self.transferPosition)
-    self.waitForCondition(self.systemModel[self.ReactorID]['transfer'].getTransfer(),self.transferPosition)
+    
+  def setMessageBox(self):
+    self.waitForUser = True
+    self.waitForCondition(self.getUserInput,True)
+    
+  def getUserInput(self):
+    return not(self.waitForUser)
     
 class DetectRadiation(UnitOperation):
   def __init__(self,systemModel,params):
@@ -306,13 +404,18 @@ class DetectRadiation(UnitOperation):
     self.doNext()
     self.getRadiation()
     self.doNext()
-  
+
   def setReactorPosition(self):
-    self.systemModel[self.ReactorID]['motion'].setPosition(RADIATION)
-    self.waitForCondition(self.systemModel[self.ReactorID]['motion'].getPosition(),RADIATION)
+    self.systemModel[self.ReactorID]['motion'].setPosition(ZDOWN)
+    self.waitForCondition(self.systemModel[self.ReactorID]['motion'].getCurrentPosition,ZDOWN,EQUAL)
+    self.systemModel[self.ReactorID]['motion'].setPosition(XRADIATION)
+    self.waitForCondition(self.systemModel[self.ReactorID]['motion'].getCurrentPosition,XRADIATION,EQUAL)
+    self.systemModel[self.ReactorID]['motion'].setPosition(ZUP)
+    self.waitForCondition(self.systemModel[self.ReactorID]['motion'].getPosition,ZUP,EQUAL)
 
   def getRadiation(self):
     self.systemModel[self.ReactorID]['radiation_detector'].getCalibratedReading()
+    self.waitForCondition(self.systemModel[self.ReactorID]['radiation_detector'].getCalibratedReading,self.calibrationCoefficient,GREATER)
     
 def test():
   react1 = React()
