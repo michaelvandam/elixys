41c9d26120db4b3ba7432d55c96a30c3e63347ea
Kevin Quinn <kquinn@mednet.ucla.edu>
2011-06-17 23:41:37 -0700

    Updated UnitOperations
    Changed functionality of waitForCondition and pausing.
    Added descriptions/timer formatting for steps of unit operations.
    Still working on the descriptions and removing old self.steps text.

--- server/Kevin/SystemModelLayer/UnitOperations.py
+++ server/Kevin/SystemModelLayer/UnitOperations.py
@@ -43,7 +43,33 @@ EQUAL = "="
 GREATER = ">"
 LESS = "<"
 
+#Private Variables by function:
+#All Unit Operations
+##self.params (List of all parameters for a unit operation)
+##self.ReactorID (ID of current reactor)
+##self.delay (Time in milliseconds to delay between polling variables for each step in unit operations -- defaults to 50ms if not changed)
+##self.isComplete (Complete flag, when set Sequence will move on to next unit operation or pause)
+##self.isRunning  (Running flag, when set Sequence will poll data and send to client)
+##self.isPaused (Pause flag, when set, unit operation will pause after next set of instructions)
+##self.stepCount (Keeps track of each step of a unit operation)
+##self.stepDescription (String that describes current step in this format "Moving to position:Moving down"
+##self.currentAction (String that describes what is happening inside the unit operation)
 
+#React Operation
+##
+##
+##
+##
+#Evaporate Operation
+##
+##
+##
+##
+#User Input Operation
+##
+##
+##
+##
 class UnitOperation(Thread):
   def __init__(self,systemModel):
     Thread.__init__(self)
@@ -51,62 +77,68 @@ class UnitOperation(Thread):
     self.systemModel = systemModel
     self.steps = ("step1","step2")
     self.currentStep = "step1"
-    self.stepCounter = 1
-    self.isRunning = True
+    self.stepCounter = 0
+    self.isRunning = False
     self.isComplete = False
-    self.pause = False
-  def setParams(params):
+    self.isPaused = False
+  def setParams(self,params): #Params come in as Dict, we can loop through and assign each 'key' to a variable. Eg. self.'key' = 'value'
     pass
-  def logError():
+  def logError(self,error):
     """Get current error from hardware comm."""
+    print error
     #error.log("error")
-    pass
     
-  def checkBeforeContinuing(self):
+  def beginNextStep(self,nextStepText = ""):
     if self.pause:
       self.paused()
-    elif self.completed:
-      print "unit operation complete"
-    else:
-      print("ERROR: Occurred in UnitOperation.checkBeforeContinuing() call.")# This will be an error log
+    if nextStepText:
+      self.stepDescription = nextStepText
+    self.setDescription()
   
   def setReactorPosition(self,reactorPosition,ReactorID = self.ReactorID):
+    storeDelayValue= self.delay #Save the current value for after this is finished.
+    self.delay = 1000 # Set delay to 1 second
     if not(self.waitForCondition(self.systemModel[ReactorID]['motion'].getCurrentXPosition,self.reactPosition,EQUAL)):
+      self.setDescription("Moving Reactor%s down." % ReactorID)
       self.systemModel[ReactorID]['motion'].setZPosition(DOWN)
       self.waitForCondition(self.systemModel[ReactorID]['motion'].getCurrentZPosition,DOWN,EQUAL)
+      self.setDescription("Moving Reactor%s to position %s." % (ReactorID,reactorPosition))
       self.systemModel[ReactorID]['motion'].setXPosition(reactorPosition)
       self.waitForCondition(self.systemModel[ReactorID]['motion'].getCurrentXPosition,reactorPosition,EQUAL)
+      self.setDescription("Moving Reactor%s up." % ReactorID)
       self.systemModel[ReactorID]['motion'].setZPosition(UP)
       self.waitForCondition(self.systemModel[ReactorID]['motion'].getCurrentZPosition,UP,EQUAL)
     else: #We're in the right position, check if we're sealed.
       if not(self.waitForCondition(self.systemModel[ReactorID]['motion'].getCurrentZPosition,UP,EQUAL)):
+        self.setDescription("Moving Reactor%s down." % ReactorID)
         self.systemModel[ReactorID]['motion'].setZPosition(UP)
         self.waitForCondition(self.systemModel[ReactorID]['motion'].getCurrentZPosition,UP,EQUAL)
       else:
-        pass #We're already in the right position and sealed.
-      
-  def waitForCondition(function,condition,comparator,timeout=3): #Default to 3 second timeout
-    timer = time.time()
+        self.setDescription("Reactor%s already in position %s." % (ReactorID,reactorPosition))
+    self.delay = storeDelayValue #Restore previous delay value
+    
+  def waitForCondition(function,condition,comparator,timeout=3): #Timeout in seconds, default to 3.
+    startTime = time.time()
     if comparator == EQUAL:
       while not(function() == condition):
-        time.sleep(0.05)#Default to 50 milliseconds
-        if isTimerExpired(timer,timeout):
-          print ("ERROR: waitForCondition call timed out on function:%s class:%s" % (function.__name__,function.im_class))
+        self.timeDelay(self.delay)
+        if isTimerExpired(startTime,timeout):
+          logError("ERROR: waitForCondition call timed out on function:%s class:%s" % (function.__name__,function.im_class))
           #ERROR
     elif comparator == GREATER:
       if not(function() >= condition):
-        time.sleep(0.05)
-        if isTimerExpired(timer,timeout):
-          print ("ERROR: waitForCondition call timed out on function:%s class:%s" % (function.__name__,function.im_class))
+        self.timeDelay(self.delay)
+        if isTimerExpired(startTime,timeout):
+          logError ("ERROR: waitForCondition call timed out on function:%s class:%s" % (function.__name__,function.im_class))
           #ERROR
     elif comparator == LESS:
       if not(function() <= condition):
-        time.sleep(0.05)
-        if isTimerExpired(timer,timeout):
-          print ("ERROR: waitForCondition call timed out on function:%s class:%s" % (function.__name__,function.im_class))
+        self.timeDelay(self.delay)
+        if isTimerExpired(startTime,timeout):
+          logError ("ERROR: waitForCondition call timed out on function:%s class:%s" % (function.__name__,function.im_class))
           #ERROR
     else:
-      print "Error: Invalid comparator."
+      LogError("Error: Invalid comparator.")
       return False
     return True
     
@@ -121,33 +153,67 @@ class UnitOperation(Thread):
       if not(function() <= condition):
         return False
     else:
-      print "Error: Invalid comparator."
+      logError("Error: Invalid comparator.")
       return False
     return True
-  def isTimerExpired(timer,timeout):
-    if (time.time()-timer >= timeout):
+    
+  def startTimer(self,timerLength): #In seconds
+    timerStartTime = time.time()  #Create a time
+    while not(isTimerExpired(timerStartTime,timerLength)):
+      setDescription("Time remaining:%s" % formatTime(timerLength-timerStartTime))
+      self.timeDelay() #Sleep 50ms between checks
+      
+  def isTimerExpired(startTime,timeout):
+    if (time.time()-startTime >= timeout):
       return True
     return False
-      
-  def timeDelay(self):
-    time.sleep(self.Timer)
+    
+  def formatTime(timeValue):
+    hours = 0
+    minutes = 0
+    seconds = 0
+    if timeValue >= 3600:
+      hours = int(timeValue/3600)
+      timeValue = timeValue % 3600 
+    if timeValue >= 60:
+      minutes = int(timeValue/60)
+      seconds = timeValue % 60 
+    if timeValue < 60:
+      seconds = int(timeValue)
+    if hours:
+      return("%.2d:%.2d:%.2d" % (hours,minutes,seconds))
+    elif minutes:
+      return("00:%.2d:%.2d" % (minutes,seconds))
+    else:
+      return("00:00:%.2d" % seconds)
+    
+  def timeDelay(self,delay=50): #delay = time in milliseconds, default to 50ms
+    if delay:
+      time.sleep(self.delay/1000.00) #If a value is set, use it, otherwise default. Divide by 1000.00 to get ms as a float.
+    else:
+      time.sleep(0.05)#default if delay = None or delay = 0
     
   def setUnpause(self):
     self.pause = False
+    
   def setPause(self):
     self.pause = True
+    
   def paused(self):
     while self.pause:
       pass
+      
   def setDescription(newDescription = ""):
     if newDescription:
-      self.description = self.currentAction+":"+newDescription
+      self.currentStepDescription = self.stepDescription+":"+newDescription
+      
   def abort():
     #Safely abort -> Do not move, turn off heaters, turn set points to zero.
     self.systemModel[self.ReactorID]['temperature_controller'].setTemperature(0)
     
   def getTotalSteps(self):
     return self.steps #Integer
+    
   def isRunning(self):
     return self.isRunning
     
@@ -172,27 +238,25 @@ class React(UnitOperation):
     self.params = params #Should have parameters listed below
     #self.ReactorID
     #self.Temp
-    #self.Timer
+    #self.time
     #self.coolTemp
     #self.reactPosition
   def run(self):
-    self.currentAction= "Starting React Operation"
-    self.checkBeforeContinuing()
-    self.currentAction= "Moving to position"
+    self.beginNextStep("Starting React Operation")
+    self.beginNextStep("Moving to position")
     self.setReactorPosition(self.reactPosition)#REACTA OR REACTB
-    self.checkBeforeContinuing()
-    self.currentAction= "Setting reactor temperature"
+    self.beginNextStep("Setting reactor temperature")
     self.setTemp()
-    self.checkBeforeContinuing()
-    self.currentAction= "Starting stir motor"
+    self.beginNextStep("Starting stir motor")
+    self.currentAction("Starting heater")
     self.setStirring()
-    self.currentAction= "Starting heater"
     self.startHeating()
-    self.timeDelay()
-    self.checkBeforeContinuing()
+    self.stepDescription("Starting timer")
+    self.startTimer(self.Time)
+    self.beginNextStep("Starting cooling")
     self.setCool()
     self.isComplete = True
-    self.checkBeforeContinuing()
+    self.beginNextStep()
 
   def setStirring(self):
     self.systemModel[self.ReactorID]['stir_motor'].setSpeed(self.stirSpeed) #Set analog value on PLC
@@ -224,28 +288,28 @@ class AddReagent(UnitOperation):
 
   def run(self):
     self.steps=["Starting add reagent operation","Moving to addition position","Adding reagent","Removing empty vial"]
-    self.checkBeforeContinuing()
+    self.beginNextStep()
     self.setReactorPosition(self.reagentPosition)
-    self.checkBeforeContinuing()
+    self.beginNextStep()
     self.setGripperPlace()
     self.timeDelay()
-    self.checkBeforeContinuing()
+    self.beginNextStep()
     self.setGripperRemove()
     self.isComplete = True
-    self.checkBeforeContinuing()
+    self.beginNextStep()
 
   def setGripperPlace():
     if self.checkForCondition(self.systemModel[self.Gripper].getGripperState,GRIP,EQUAL):
       if self.checkForCondition(self.systemModel[self.Gripper].getCurrentZPosition,DOWN,EQUAL):
         if (self.checkForCondition(self.systemModel[self.Gripper].getCoordinate,LOAD_A,EQUAL) or self.checkForCondition(self.systemModel[self.Gripper].getCoordinate,LOAD_B,EQUAL)):
           #We are in the load position, we should not be here. Error.
-          print "ERROR: In load position, calling setGripperPlace()"
+          logError("ERROR: In load position, calling setGripperPlace()")
         else:
           #We are gripped and down, just ungrip?
-          print "ERROR: setGripperPlace() called while already gripped in down position."
+          logError("ERROR: setGripperPlace() called while already gripped in down position.")
       else:
         #Error, why are we already gripped and not in a down position? 
-        print "ERROR: setGripperPlace() called while already gripped in raised position."
+        logError("ERROR: setGripperPlace() called while already gripped in raised position.")
     if not(self.waitCheckCondition(self.systemModel[self.Gripper].getCoordinate,self.ReagentID,EQUAL)):
       self.systemModel[self.Gripper].setZPosition(UP)
       self.waitForCondition(self.systemModel[self.Gripper].getCurrentZPosition,UP,EQUAL)
@@ -286,9 +350,9 @@ class AddReagent(UnitOperation):
         self.systemModel[self.Gripper].setCoordinate(HOME)
         self.waitForCondition(self.systemModel[self.Gripper].getCoordinate,HOME,EQUAL)
       else:
-        print "Gripper in correct position for removal, but not in down position."
+        logError("Gripper in correct position for removal, but not in down position.")
     else:
-      print "Gripper not in correct position for removal when setGripperRemove() called."
+      logError("Gripper not in correct position for removal when setGripperRemove() called.")
 
 class Evaporate(UnitOperation):
   def __init__(self,systemModel,params):
@@ -296,26 +360,26 @@ class Evaporate(UnitOperation):
     self.params = params #Should have parameters listed below
     #self.ReactorID
     #self.Temp
-    #self.Timer
+    #self.Time
     #self.coolTemp
     #self.stirSpeed
     
   def run(self):
-    self.checkBeforeContinuing()
+    self.beginNextStep()
     self.setReactorPosition(EVAPORATE)
-    self.checkBeforeContinuing()
+    self.beginNextStep()
     self.setTemp()
-    self.checkBeforeContinuing()
+    self.beginNextStep()
     startVacuum()
     self.startStirring()
     self.startHeating()
-    self.timeDelay()
-    self.checkBeforeContinuing()
+    self.startTimer()
+    self.beginNextStep()
     self.setCool()
     self.stopStirring()
     self.stopVacuum()
     self.isComplete = True
-    self.checkBeforeContinuing()
+    self.beginNextStep()
     
   def startStirring(self):
     self.systemModel[self.ReactorID]['stir_motor'].setSpeed(self.stirSpeed) #Set analog value on PLC
@@ -357,9 +421,6 @@ class Evaporate(UnitOperation):
   def getTemp(self):
     self.systemModel[self.ReactorID]['temperature_controller'].getCurrentTemperature()
     
-  def timeDelay(self):
-    self.timeDelay(self.Time)
-    
 class InstallVial(UnitOperation):
   def __init__(self,systemModel,params):
     UnitOperation.__init__(self,systemModel)
@@ -367,10 +428,10 @@ class InstallVial(UnitOperation):
     #self.ReactorID
   def run(self):
     self.steps=["Starting install vial operation","Moving to vial installation position"]
-    self.checkBeforeContinuing()
+    self.beginNextStep()
     self.setReactorPosition(INSTALL)
     self.isComplete = True
-    self.checkBeforeContinuing()
+    self.beginNextStep()
     
 class TransferToHPLC(UnitOperation):
   def __init__(self,systemModel,params):
@@ -380,22 +441,22 @@ class TransferToHPLC(UnitOperation):
     
   def run(self):
     self.steps=["Starting HPLC Transfer operation","Moving to transfer position","Transferring product to HPLC","Switching injection valve"]
-    self.checkBeforeContinuing()
+    self.beginNextStep()
     self.setReactorPosition(TRANSFER)
-    self.checkBeforeContinuing()
+    self.beginNextStep()
     self.setTransfer()
-    self.checkBeforeContinuing()
-    self.setHPLC()
+    self.beginNextStep()
+    self.setHPLCValve()
     self.isComplete = True
-    self.checkBeforeContinuing()
+    self.beginNextStep()
 
   def setTransfer(self):
     self.systemModel[self.ReactorID]['transfer'].setTransfer(HPLC)
-    self.waitForCondition(self.systemModel[self.ReactorID]['transfer'].getTransfer(),HPLC)
+    self.waitForCondition(self.systemModel[self.ReactorID]['transfer'].getTransfer(),HPLC,EQUAL)
     
-  def setHPLC(self):
+  def setHPLCValve(self):
     self.systemModel[self.ReactorID]['transfer'].setHPLC(INJECT)       
-    self.waitForCondition(self.systemModel[self.ReactorID]['transfer'].getHPLC(),INJECT) 
+    self.waitForCondition(self.systemModel[self.ReactorID]['transfer'].getHPLC(),INJECT,EQUAL) 
   
 class TransferElute(UnitOperation):
   def __init__(self,systemModel,params):
@@ -405,23 +466,22 @@ class TransferElute(UnitOperation):
     #self.stopcockPosition
     
   def run(self):
-    self.steps=["Starting Transfer operation","Moving to transfer position","Setting stopcock position","Beginning transfer"]
-    self.checkBeforeContinuing()
+    self.beginNextStep()
     self.setReactorPosition(TRANSFER)
-    self.checkBeforeContinuing()
+    self.beginNextStep()
     self.setStopcock()
-    self.checkBeforeContinuing()
+    self.beginNextStep()
     self.setTransfer()
     self.isComplete = True
-    self.checkBeforeContinuing()
+    self.beginNextStep()
   
   def setStopcock(self):
     self.systemModel[self.ReactorID]['stopcock'].setStopcock(self.stopcockPosition)       
-    self.waitForCondition(self.systemModel[self.ReactorID]['stopcock'].getStopcock(),self.stopcockPosition) 
+    self.waitForCondition(self.systemModel[self.ReactorID]['stopcock'].getStopcock(),self.stopcockPosition,EQUAL) 
     
   def setTransfer(self):
     self.systemModel[self.ReactorID]['transfer'].setTransfer(self.transferPosition)
-    self.waitForCondition(self.systemModel[self.ReactorID]['transfer'].getTransfer(),self.transferPosition)
+    self.waitForCondition(self.systemModel[self.ReactorID]['transfer'].getTransfer(),self.transferPosition,EQUAL)
    
 class Transfer(UnitOperation):
   def __init__(self,systemModel,params):
@@ -432,13 +492,13 @@ class Transfer(UnitOperation):
     
   def run(self):
     self.steps=["Starting Transfer operation","Moving to transfer position","Transferring product"]
-    self.checkBeforeContinuing()
+    self.beginNextStep()
     self.setReactorPosition(TRANSFER)
-    self.checkBeforeContinuing()
+    self.beginNextStep()
     self.setReactorPosition(ADDREAGENT,self.transferReactorID)
     self.startTransfer()
     self.isComplete = True
-    self.checkBeforeContinuing()
+    self.beginNextStep()
 
   def startTransfer(self):
     self.systemModel[self.ReactorID]['transfer'].setTransfer(self.transferPosition)
@@ -454,11 +514,11 @@ class UserInput(UnitOperation):
     #self.description
   def run(self):
     self.currentAction="Starting User Input operation"
-    self.checkBeforeContinuing()
+    self.beginNextStep()
     self.currentAction="Message box"
     self.setMessageBox()
     self.isComplete = True
-    self.checkBeforeContinuing()
+    self.beginNextStep()
     
   def setMessageBox(self):
     self.setDescription("Waiting for user input")
@@ -477,12 +537,12 @@ class DetectRadiation(UnitOperation):
     
   def run(self):
     self.steps=["Starting radiation detector operation","Moving to radiation detector position","Detecting radiation"]
-    self.checkBeforeContinuing()
+    self.beginNextStep()
     self.setReactorPosition(RADIATION)
-    self.checkBeforeContinuing()
+    self.beginNextStep()
     self.getRadiation()
     self.isComplete = True
-    self.checkBeforeContinuing()
+    self.beginNextStep()
 
 
   def getRadiation(self):
