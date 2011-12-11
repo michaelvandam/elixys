77c7de350b6c7baf402ec1c04d8eb91e2dd072d0
Shane Claggett <shaneclaggett@gmail.com>
2011-12-10 19:24:17 -0800

    Added missing binaries and configuration files to rtmpd.

--- server/rtmpd/crtmpdemoserver.lua
+++ server/rtmpd/crtmpdemoserver.lua
@@ -37,44 +37,9 @@ configuration=
 			},
 			acceptors = 
 			{
-				{
-					ip="0.0.0.0",
-					port=6666,
-					protocol="inboundLiveFlv",
-					waitForMetadata=true,
-				},
-				{
-					ip="0.0.0.0",
-					port=9999,
-					protocol="inboundTcpTs"
-				},
-				--[[{
-					ip="0.0.0.0",
-					port=7654,
-					protocol="inboundRawHttpStream",
-					crossDomainFile="/tmp/crossdomain.xml"
-				},
-				{
-					ip="0.0.0.0",
-					port=554,
-					protocol="inboundRtsp"
-				},]]--
 			},
 			externalStreams = 
 			{
-				--[[
-				{
-					uri="rtsp://fms20.mediadirect.ro/live2/realitatea/realitatea",
-					localStreamName="rtsp_test",
-					forceTcp=true
-				},
-				{
-					uri="rtmp://edge01.fms.dutchview.nl/botr/bunny",
-					localStreamName="rtmp_test",
-					swfUrl="http://www.example.com/example.swf";
-					pageUrl="http://www.example.com/";
-					emulateUserAgent="MAC 10,1,82,76",
-				}]]--
 			},
 			validateHandshake=true,
 			keyframeSeek=true,
