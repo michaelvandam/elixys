a8bc771bd40726b992a3ab97214f3cbb54444eb8
henry <henry.herman@gmail.com>
2013-05-13 16:00:56 -0700

    Fixed debian database config

--- server/config/elixys_install_database.sh
+++ server/config/elixys_install_database.sh
@@ -13,22 +13,26 @@ source elixys_paths.sh
 
 cd $ELIXYS_CONFIG_PATH
 
-# Install mysql config
-if [ -f /etc/mysql/my.cnf ]; then
-	mv /etc/mysql/my.cnf /etc/mysql/my.cnf.bkup
-fi
-cp my.cnf /etc/mysql/
-
+service mysql stop
+mysqld --skip-grant-tables &
+SQLPID=$!
+echo $SQLPID > mysql_nopw.pid
+sleep 3
 # Create the database, tables and stored procedures
 echo "Recreating database..."
 mysql -e "DROP DATABASE IF EXISTS Elixys;"
 mysql -e "CREATE DATABASE Elixys;"
+mysql < AllowRootAccess.sql
+kill -KILL $SQLPID
+killall mysqld
+service mysql start
+sleep 3
 mysql Elixys < SetDatabasePrivileges.sql
 mysql Elixys < CreateDatabaseTables.sql
 mysql Elixys < CreateDatabaseProcedures.sql
 
 # Restart the database
 echo "Restarting database..."
-/sbin/service mysqld restart
-echo "Done"
+service mysql restart
+#echo "Done"
 
