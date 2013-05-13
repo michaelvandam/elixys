29d6ec448e754b2f4b9d16d5b7fd5b578df6e3b8
henry <henry.herman@gmail.com>
2013-05-13 14:21:54 -0700

    Added Git update

--- server/config/elixys_install.sh
+++ server/config/elixys_install.sh
@@ -18,7 +18,18 @@ rm -Rf $ELIXYS_SRC
 rm -Rf $ELIXYS_INSTALL_PATH/*
 
 # Grab the source
-git clone --depth 1 $ELIXYS_REPO $ELIXYS_SRC
+if [ -d $ELIXYS_SRC ] 
+then
+# Git pull most up-to-date code
+	echo "Update the Elixys repo"
+	cd $ELIXYS_SRC
+	git remote update
+else
+	# Git Clone the Repo
+	echo "Clone the Elixys Repo"
+	git clone --depth 1 -b deb-install $ELIXYS_REPO $ELIXYS_SRC
+	cd $ELIXYS_SRC
+fi
 
 # Create the Elixys Install Directory
 mkdir -p $ELIXYS_INSTALL_PATH
@@ -26,7 +37,6 @@ mkdir -p $ELIXYS_LOG_PATH
 mkdir -p $ELIXYS_CONFIG_PATH
 
 # Copy the default configuration and setup scripts
-cd $ELIXYS_SRC
 cp -R server/config/* $ELIXYS_CONFIG_PATH 
 cd $ELIXYS_CONFIG_PATH
 
