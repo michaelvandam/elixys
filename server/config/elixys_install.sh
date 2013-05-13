b586faeb560dfba1ae4cde8fb4999f105063f6ef
henry <henry.herman@gmail.com>
2013-05-13 14:35:14 -0700

    Database install script

--- server/config/elixys_install.sh
+++ server/config/elixys_install.sh
@@ -4,14 +4,9 @@ if [ "$(whoami)" != "root" ]; then
 	echo "Elixys server install require root priveleges"
 	exit 1
 fi
+./elixys_install_deps.sh
+source elixys_paths.sh
 
-# Global Paths and Variable
-export ELIXYS_SRC=/usr/local/src/elixys
-export ELIXYS_REPO=https://github.com/henryeherman/elixys.git
-export ELIXYS_INSTALL_PATH=/opt/elixys
-export ELIXYS_CONFIG_PATH=$ELIXYS_INSTALL_PATH/config
-export ELIXYS_LOG_PATH=$ELIXYS_INSTALL_PATH/log
-export ELIXYS_RTMPD_PATH=$ELIXYS_INSTALL_PATH/rtmpd
 # Remove old src
 rm -Rf $ELIXYS_SRC
 # Remove old configs
@@ -54,13 +49,9 @@ cp $ELIXYS_SRC/server/config/adobepolicyfile/crossdomain.xml \
 	/var/www/adobepolicyfile
 chmod 444 /var/www/adobepolicyfile/crossdomain.xml
 
-# Install mysql config
-if [ -f /etc/mysql/my.cnf ]; then
-	mv /etc/mysql/my.cnf /etc/mysql/my.cnf.bkup
-fi
-cp my.cnf /etc/mysql/
+
 service mysql restart
-./InitializeDatabase.sh
+./elixys_install_database.sh
 
 # Copy over the crtmp server scripts
 #cp $ELIXYS_SRC/server/rtmpd/* $ELIXYS_RTMPD_PATH
