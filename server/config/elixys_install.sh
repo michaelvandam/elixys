8b1791f01311ab57cf79f746ec3d2b45a1ce4dff
Henry <henry.herman@gmail.com>
2013-05-14 13:14:00 -0700

    Working Debian install

--- server/config/elixys_install.sh
+++ server/config/elixys_install.sh
@@ -5,47 +5,14 @@ if [ "$(whoami)" != "root" ]; then
 	exit 1
 fi
 
-#Install Elixys Dependencies
-apt-get update
-export DEBIAN_FRONTEND=noninteractive
-apt-get -q -y install python python-mysqldb python-mysql.connector \
-		 python-configobj python-setuptools python-pip \
-		 ipython ipython-notebook-common ipython-qtconsole
-pip install rpyc
-pip install twilio
-apt-get -q -y install vim git-core binutils
-apt-get -q -y install autojump
-echo "source /usr/share/autojump/autojump.bash" >> ~/.bashrc
-apt-get -q -y install mysql-common mysql-server
-apt-get -q -y install apache2 apache2-utils \
-			libapache2-mod-wsgi apache2-threaded-dev \
-			libapache2-mod-auth-mysql \
-			libaprutil1-dbd-mysql 
-			# \ apache2-prefork-dev
-
-sudo apt-get -q -y install crtmpserver
-
-export ADOBE_POL_PATH_SRC=/usr/local/src/mod_adobe_crossdomainpolicy
-rm -Rf $ADOBE_POL_PATH_SRC
-mkdir -p $ADOBE_POL_PATH_SRC
-cd $ADOBE_POL_PATH_SRC
-wget http://www.beamartyr.net/projects/mod_adobe_crossdomainpolicy.c
-apxs2 -cia mod_adobe_crossdomainpolicy.c
-
-a2enmod adobe_crossdomainpolicy
-a2enmod wsgi
-a2enmod authn_dbd
-a2enmod authz_host
-a2enmod authz_user
-a2enmod rewrite
-a2enmod auth_mysql
+./elixys_install_deps.sh
 
 source elixys_paths.sh
 
 # Remove old src
 #rm -Rf $ELIXYS_SRC
 # Remove old configs
-rm -Rf $ELIXYS_INSTALL_PATH/*
+rm -Rf $ELIXYS_INSTALL_PATH
 
 # Grab the source
 if [ -d $ELIXYS_SRC ] 
@@ -86,7 +53,7 @@ cp $ELIXYS_SRC/server/config/adobepolicyfile/crossdomain.xml \
 	/var/www/adobepolicyfile
 chmod 444 /var/www/adobepolicyfile/crossdomain.xml
 
-cd $ELIXYS_CONFIG_PATH/deb_install
+cd $ELIXYS_CONFIG_PATH
 ./elixys_install_database.sh
 
 # Copy over the crtmp server scripts
@@ -100,7 +67,7 @@ cp $ELIXYS_SRC/server/config/elixys-web /etc/apache2/sites-available
 a2dissite default
 a2ensite elixys-web
 
-cd $ELIXYS_CONFIG_PATH/deb_install
+cd $ELIXYS_CONFIG_PATH
 ./elixys_update.sh
-cd $ELIXYS_CONFIG_PATH/deb_install
+cd $ELIXYS_CONFIG_PATH
 ./elixys_setup_demo.sh
