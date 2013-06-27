716b679cbecac83d1779483498da72c0f8b5b414
henry <henry.herman@gmail.com>
2013-06-27 12:28:17 -0700

    Browser Streaming tests

--- server/config/setup_cameras.sh
+++ server/config/setup_cameras.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-sudo apt-get install -y -q crtmpserver 
-sudo apt-get install -y -q supervisor 
+apt-get install -y -q crtmpserver 
+apt-get install -y -q supervisor 
 
 patch /etc/init.d/supervisor < supervisor.patch
 
-sudo ln -s /opt/elixys/config/elixyssupervisord.conf  \ 
-        /etc/supervisor/conf.d/
+ln -s /opt/elixys/config/elixyssupervisord.conf  \ 
+        /etc/supervisor/conf.d
 
-sudo service crtmpserver restart
-sudo service supervisor restart
+service crtmpserver restart
+service supervisor restart
