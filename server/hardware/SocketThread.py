b740ee6e6e527cbd57e5a69074c513230350f997
Luis Fuentes <luis90fuentes@gmail.com>
2013-10-29 19:07:10 -0700

    PDF Batch Report Updated and Added error logging for any socket packets sent out.

--- server/hardware/SocketThread.py
+++ server/hardware/SocketThread.py
@@ -7,6 +7,14 @@ import socket
 import threading
 import select
 import Queue
+# Import loging capabilities
+import logging.config
+import logging
+import traceback
+
+logging.config.fileConfig("/opt/elixys/config/elixyslog.conf")
+log = logging.getLogger("elixys.hw")
+log.debug("Starting Socket Thread -- Socket Thread")
 
 # This exception will be thrown in a runaway heater is detected
 class RunawayHeaterException(Exception):
@@ -39,6 +47,7 @@ class SocketThread(threading.Thread):
 
     # Thread function
     def run(self):
+        log.debug("Starting a socket thread run")
         try:
             # Create a socket connection to the PLC
             pSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -50,8 +59,10 @@ class SocketThread(threading.Thread):
             # Loop until the terminate event is set
             bRunawayHeater = False
             while not self.__pTerminateEvent.is_set():
-                # Raise an exception if a runaway heater was detected and we've sent all our outgoing packets
+                # Raise an exception if a runaway heater was detected
+                # and we've sent all our outgoing packets
                 if bRunawayHeater and self.__pOutgoingPackets.empty():
+                    log.error("Runaway Heater detected and all Packets were sent out.")
                     raise Exception(sRunawayHeaterError)
 
                 # Call select() to wait until the socket is ready
@@ -60,24 +71,36 @@ class SocketThread(threading.Thread):
                 else:
                     pReadySockets = select.select(pSockets, pSockets, [], 0.05)
                 if len(pReadySockets[0]) > 0:
-                    # The socket has data available to be read.  Pass the data to the HardwareComm for processing
+                    # The socket has data available to be read.
+                    # Pass the data to the HardwareComm for processing
+                    log.debug("Reading Socket Thread packets")
                     pBinaryResponse = pSocket.recv(10240)
                     sResponse = pBinaryResponse.encode("hex")
                     try:
                         self.__pHardwareComm._HardwareComm__ProcessRawResponse(sResponse)
                     except RunawayHeaterException, ex:
-                        # A runaway heater has been detected.  We need to run the message pump a bit longer so the queued heater stop commands can be sent
+                        # A runaway heater has been detected.
+                        # We need to run the message pump a bit longer
+                        # so the queued heater stop commands can be sent.
+                        log.error("Caught exception: Runaway Heater was detected.\
+                                    Queued the stop heater commands.")
+                        log.error("Exception: %s" %ex)
                         bRunawayHeater = True
                         sRunawayHeaterError = "Runaway heater in reactor " + str(ex.args[0])
                 elif len(pReadySockets[1]) > 0:
-                    # The socket is available for writing.  Do we have any packets in our outgoing queue?
+                    # The socket is available for writing.
+                    # Do we have any packets in our outgoing queue?
                     if not self.__pOutgoingPackets.empty():
-                       # Yes, so pop the next one off and send it
-                       pSocket.sendto(self.__pOutgoingPackets.get(), (self.__sPLCIP, self.__nPLCPort))
+                        log.debug("Writing Socket Thread packets")
+                        # Yes, so pop the next one off and send it
+                        pSocket.sendto(self.__pOutgoingPackets.get(),
+                                    (self.__sPLCIP, self.__nPLCPort))
 
             # Close the socket connection
             pSocket.close()
             pSocket = None
+            log.debug("Finished socket thread session")
         except Exception, ex:
+            log.error("Exception caught in Socket Thread: %s" % ex)
+            log.error("Traceback:\r\n%s\r\n" % traceback.format_exc())
             self.__sError = str(ex)
-
